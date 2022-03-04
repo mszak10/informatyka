67c871197d58676aeaa19a6a3cbcd9284b2106e4
mszak10 <49935726+mszak10@users.noreply.github.com>
2022-03-04 13:36:24 +0100

    4.2 Styczeń 2022

--- Styczeń 2022/main.py
+++ Styczeń 2022/main.py
@@ -1,13 +1,39 @@
 # Wyznacz liczbę miast w której powstają galerie
 
+# Zadanie 4.1
 kraje = {}
-with open("Styczeń 2022/galerie.txt") as plik:
+with open("galerie.txt") as plik:
     for linia in plik:
         kraj = linia.split(" ")[0]
-        if kraje.__contains__(kraj):
+        if kraj in kraje:
             kraje[kraj] += 1
         else:
             kraje[kraj] = 1
-with open("Styczeń 2022/4_1.txt", 'w') as plik:
+with open("wynik4_1.txt.txt", 'w') as plik:
     for kraj in kraje:
         plik.write(kraj + " " + str(kraje[kraj]) + "\n")
+
+# Zadanie 4.2
+with open("galerie.txt") as plik:
+    maximum = ['First', 0]
+    minimum = ['Last', 10000]
+    for linia in plik:
+        a = count = 0
+        linia = linia.split(" ")
+        miasto = linia[1]
+        for i in range(3, len(linia), 2):
+            a += int(linia[i-1]) * int(linia[i])
+            if int(linia[i-1]) * int(linia[i]) > 0:
+                count += 1
+        if a > maximum[1]:
+            maximum[0] = miasto
+            maximum[1] = a
+        elif a <= minimum[1]:
+            minimum[0] = miasto
+            minimum[1] = a
+        # print(f"{miasto} {a} {count}")
+        with open("wynik4_2a.txt", 'a') as p:
+            p.write(miasto + " " + str(a) + " " + str(count) + "\n")
+    with open("wynik4_2b.txt", 'a') as r:
+        r.write(f'{maximum[0]} {maximum[1]}\n')
+        r.write(f'{minimum[0]} {minimum[1]}')
