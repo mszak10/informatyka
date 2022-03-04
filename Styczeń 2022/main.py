5f6ea3fcea6c74bfaed94cb098a3735d14b558f8
mszak10 <49935726+mszak10@users.noreply.github.com>
2022-03-04 16:01:22 +0100

    4.3 Styczeń 2022

--- Styczeń 2022/main.py
+++ Styczeń 2022/main.py
@@ -1,4 +1,4 @@
-# Wyznacz liczbę miast w której powstają galerie
+# https://zadaniezinformatyki.pl/matura-2021-galerie-handlowe/
 
 # Zadanie 4.1
 kraje = {}
@@ -31,9 +31,35 @@ with open("galerie.txt") as plik:
         elif a <= minimum[1]:
             minimum[0] = miasto
             minimum[1] = a
-        # print(f"{miasto} {a} {count}")
         with open("wynik4_2a.txt", 'a') as p:
             p.write(miasto + " " + str(a) + " " + str(count) + "\n")
-    with open("wynik4_2b.txt", 'a') as r:
+    with open("wynik4_2b.txt", 'w') as r:
+        r.write(f'{maximum[0]} {maximum[1]}\n')
+        r.write(f'{minimum[0]} {minimum[1]}')
+
+# Zadanie 4.3
+with open("galerie.txt") as plik:
+    lokale = []
+    for linia in plik:
+        linia = linia.split(" ")
+        a = 0
+        area = []
+        miasto = linia[1]
+        for i in range(3, len(linia), 2):
+            a += int(linia[i-1]) * int(linia[i])
+            if int(linia[i-1]) * int(linia[i]) > 0:
+                area += [a]
+                a = 0
+        lokale += [miasto, len(set(area))]
+    maximum = ['First', 0]
+    minimum = ['Last', 10000]
+    for i in range(0, len(lokale), 2):
+        if lokale[i+1] > maximum[1]:
+            maximum[1] = lokale[i+1]
+            maximum[0] = lokale[i]
+        elif lokale[i+1] < minimum[1]:
+            minimum[1] = lokale[i + 1]
+            minimum[0] = lokale[i]
+    with open("wynik4_3.txt", 'w') as r:
         r.write(f'{maximum[0]} {maximum[1]}\n')
         r.write(f'{minimum[0]} {minimum[1]}')
