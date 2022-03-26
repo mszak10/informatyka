18ce087b43f6849b8bd5cc6fce6a2cea2b117ca7
mszak10 <mszak10@gmail.com>
2022-03-26 14:20:59 +0100

    Styczeń 2022 - 4.2

--- Styczeń 2022_2/main.py
+++ Styczeń 2022_2/main.py
@@ -1,9 +1,9 @@
 # Zadanie 4 - Wykreślanka
 
+print('4.1')
 with open("pliki/wykreslanka.txt") as file:
     fileContent = file.read()
     fileContent = fileContent.split('\n')
-    # print(len(fileContent))
 
     for i, linia in enumerate(fileContent):
         if 'matura' in linia:
@@ -24,4 +24,28 @@ print('===============')
 for i, linia in enumerate(rotated):
     if 'matura' in linia:
         index = linia.find("matura")
-        print(f'{i}') # missing 199
+        print(f'{i}')  # warn missing 199
+
+print('4.2')
+with open("pliki/wykreslanka.txt") as file:
+    result = 0
+    fileContent = file.read()
+    fileContent = fileContent.split('\n')
+    c = 1
+    rows = []
+    for i, linia in enumerate(fileContent):
+        for j in range(len(fileContent)):
+            while True:
+                if fileContent[i][j] == fileContent[i][j + 1]:
+                    c += 1
+                    break
+                else:
+                    if c == result:  # DEBUG: to be checked
+                        rows.append(i)
+                    elif c > result:
+                        result = c
+                        rows.clear()
+                        rows.append(i)
+                    c = 1
+                    break
+    print(result, rows)
